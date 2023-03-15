8bd447262607f185dc49f66bd4a1c3704e59871d
JOSEPH <lena.boya26@gmail.com>
2023-03-15 12:04:43 +0100

    show

--- src/Pages/Forms.jsx
+++ src/Pages/Forms.jsx
@@ -8,142 +8,166 @@ import Logo from '../assets/img1.png';
 const HomePage = () => {
   const [showArrow, setShowArrow] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const [showForm, setShowForm] = useState(false);
 
 
 
-    useLayoutEffect(() => {
-        window.scrollTo(0, 0);
-      }, []);
-    
-      const handleScroll = () => {
-        if (window.pageYOffset > 300) {
-          setShowArrow(true);
-        } else {
-          setShowArrow(false);
-        }
-      };
-      
-      useEffect(() => {
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-      }, []);
-      
-      const scrollToTop = () => {
-        window.scrollTo({ top: 0, behavior: "smooth" });
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const handleScroll = () => {
+    if (window.pageYOffset > 300) {
+      setShowArrow(true);
+    } else {
+      setShowArrow(false);
+    }
   };
-  
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
     }, 2000);
   }, []);
-  
-      
-  return (
-    
-    <>
-    {isLoading ? (
-      <div className="loader-container">
-        <div className="loader" />
-      </div>
-    ) : (
-      <>
 
-       <div
-        onClick={scrollToTop}
-        style={{
-          display: showArrow ? "block" : "none",
-          position: "fixed",
-          bottom: "20px",
-          right: "20px",
-          zIndex: 1000,
-          cursor: "pointer",
-        }}
-      >
-        <BsArrowUpSquare style={{ fontSize: "2rem", color: "#333" }} />
-      </div>
-        <div className="header">
+  const handleClick = () => {
+    setShowForm(!showForm);
+  };
+  
+
+  return (
+
+    <>
+      {isLoading ? (
+        <div className="loader-container">
+          <div className="loader" />
+        </div>
+      ) : (
+        <>
+
+          <div
+            onClick={scrollToTop}
+            style={{
+              display: showArrow ? "block" : "none",
+              position: "fixed",
+              bottom: "20px",
+              right: "20px",
+              zIndex: 1000,
+              cursor: "pointer",
+            }}
+          >
+            <BsArrowUpSquare style={{ fontSize: "2rem", color: "#333" }} />
+          </div>
+          <div className="header">
             <img src={Logo} alt="logo" id="img1" className="header-img" />
             <h1 className="header-text title">
-                Choisir sa forme juridique <br />
-                en 2 minutes!
+              Choisir sa forme juridique <br />
+              en 2 minutes!
             </h1>
-    </div>
-    <div className='typewriter-container'>
-        <Typewriter
-          typing={1}
-          speed={500} // Augmentez ou diminuez cette valeur pour régler la vitesse de frappe
-          onTypingDone={() => console.log('Typing done!')}
-        >
-          <h2>
-            Vous souhaitez créer votre entreprise mais vous ne savez pas quelle
-            forme juridique choisir? Grace à ce simulateur, vous saurez quelle
-            forme juridique est la plus adaptée à votre activité, à votre
-            situation personnelle et à vos besoins ! Petit bonus, possibilité
-            d'avoir le budget prévisionnel de la forme juridique la mieux adapté
-            pour les banques!
-          </h2>
-      </Typewriter>
-      </div>
-        <div className="box">
+          </div>
+          <div className='typewriter-container'>
+            <Typewriter
+              typing={1}
+              speed={500} // Augmentez ou diminuez cette valeur pour régler la vitesse de frappe
+              onTypingDone={() => console.log('Typing done!')}
+            >
+              <h2>
+                Vous souhaitez créer votre entreprise mais vous ne savez pas quelle
+                forme juridique choisir? Grace à ce simulateur, vous saurez quelle
+                forme juridique est la plus adaptée à votre activité, à votre
+                situation personnelle et à vos besoins ! Petit bonus, possibilité
+                d'avoir le budget prévisionnel de la forme juridique la mieux adapté
+                pour les banques!
+              </h2>
+            </Typewriter>
+            </div>
+            <br />
+            <div className="button-container">
+            <button className="custom-button"  onClick={handleClick}>
+  Commencer la Simulation
+              </button>
+            </div>
+            {showForm && (
+          <div className="box">
 
 
+
+
+            <br />
+            <br />
            
-
-            <br />
-            <br />
-            
-            <div className="input-container">
-                <label htmlFor="name" className="input-label">Votre Activité</label>
-                <input type="text" placeholder="Activité" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
-                <br />
-                <br />
-                <p>Votre Chiffre d'affaires annuel</p>
-                <input type="text" placeholder="Chiffre d'affaires annuel" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
-                <br />
-                <br />
-                <input type="text" placeholder="Avez-vous une location professionnelle? Si oui, pour quel montant annuel?" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
-                <br />
-                <br />
-                <input type="text" placeholder="Présence de salarié oui ou non" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
-                <br />
-                <br />
-                <input type="text" placeholder="Indiquez le nombre de salarié" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
-                <br />
-                <br />
-                <input type="text" placeholder="Salaire brut mensuel Salarié 1" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
-                <br />
-                <br />
-                <input type="text" placeholder="Votre rémunération net mensuel (ou retrait personnel mensuel)" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
-                <br />
-                <br />
-                <input type="text" placeholder="Investissement : matériels Indiquez le montant" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
-                <br />
-                <br />
-                <input type="text" placeholder="Investissement: véhicule" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
-                <br />
-                <br />
-                <input type="text" placeholder="Indiquez le nombre de Véhicule de tourisme (VP)" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
-                <br />
-                <br />
-                <input type="text" placeholder="Indiquez le nombre de Véhicule utilitaire (VU)" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
-                <br />
-                <br />
-                <input type="text" placeholder="Indiquez le prix du VP 1" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
-                <br />
-                <br />
-                <input type="text" placeholder="Indiquez le taux d'émission de CO2 VP 1" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
-                <br />
-                <br />
-        </div>
-
-    </div>
-
-          </>
-        )}
+            <div className="input-container ">
+              <label htmlFor="name" className="input-label">Votre Activité</label>
+              <input type="text" placeholder="Activité" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
+              <br />
+              <br />
+              <p>Votre Chiffre d'affaires annuel</p>
+              <input type="text" placeholder="Chiffre d'affaires annuel" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
+              <br />
+                  <br />
+                  <label htmlFor="name" className="input-label">Avez-vous une location professionnelle? Si oui, pour quel montant annuel?</label>
+              <input type="text" placeholder="Avez-vous une location professionnelle? Si oui, pour quel montant annuel?" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
+              <br />
+                  <br />
+                  <label htmlFor="name" className="input-label">Présence de salarié oui ou non</label>
+              <input type="text" placeholder="Présence de salarié oui ou non" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
+              <br />
+                  <br />
+                  <label htmlFor="name" className="input-label">Indiquez le nombre de salarié</label>
+              <input type="text" placeholder="Indiquez le nombre de salarié" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
+              <br />
+                  <br />
+                  <label htmlFor="name" className="input-label">Salaire brut mensuel Salarié 1</label>
+              <input type="text" placeholder="Salaire brut mensuel Salarié 1" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
+              <br />
+                  <br />
+                  <label htmlFor="name" className="input-label">Votre rémunération net mensuel (ou retrait personnel mensuel)</label>
+              <input type="text" placeholder="Votre rémunération net mensuel (ou retrait personnel mensuel)" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
+              <br />
+                  <br />
+                  <label htmlFor="name" className="input-label">Investissement : matériels Indiquez le montant</label>
+              <input type="text" placeholder="Investissement : matériels Indiquez le montant" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
+              <br />
+                  <br />
+                  <label htmlFor="name" className="input-label">Investissement: véhicule</label>
+              <input type="text" placeholder="Investissement: véhicule" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
+              <br />
+                  <br />
+                  <label htmlFor="name" className="input-label">Indiquez le nombre de Véhicule de tourisme (VP)</label>
+              <input type="text" placeholder="Indiquez le nombre de Véhicule de tourisme (VP)" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
+              <br />
+                  <br />
+                  <label htmlFor="name" className="input-label">Indiquez le nombre de Véhicule utilitaire (VU)</label>
+              <input type="text" placeholder="Indiquez le nombre de Véhicule utilitaire (VU)" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
+              <br />
+                  <br />
+                  <label htmlFor="name" className="input-label">Indiquez le prix du VP 1</label>
+              <input type="text" placeholder="Indiquez le prix du VP 1" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
+              <br />
+                  <br />
+                  <label htmlFor="name" className="input-label">Indiquez le taux d'émission de CO2 VP 1</label>
+              <input type="text" placeholder="Indiquez le taux d'émission de CO2 VP 1" style={{ backgroundColor: 'rgba(75, 0, 130, 0.1)', height: '40px' }} />
+              <br />
+              <br />
+                </div>
+                
+           
+              </div>
+               )}
+        </>
+      )}
     </>
   );
 };
-
 
 export default HomePage;
