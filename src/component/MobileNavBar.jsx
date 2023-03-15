bf3e0a5422ffebc92dd7cde3b45e21df45ddf7b4
JOSEPH <lena.boya26@gmail.com>
2023-03-15 16:20:39 +0100

    navMobile

---
+++ src/component/MobileNavBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { AiOutlineHome } from 'react-icons/ai';
+import { GrNotes } from 'react-icons/gr';
+import { BsCalendar3 } from 'react-icons/bs';
+
+function MobileNavBar() {
+  return (
+    <div className="fixed bottom-0 left-0 w-full p-4 md:hidden bg-white rounded-t-lg shadow-lg flex justify-around items-center">
+      <a href="#" className="focus:outline-none flex flex-col items-center">
+        <AiOutlineHome className="text-2xl" />
+        <span className="text-sm">Accueil</span>
+      </a>
+      <a href="#" className="focus:outline-none flex flex-col items-center">
+        <GrNotes className="text-2xl" />
+        <span className="text-sm">Contact</span>
+      </a>
+      <a href="#" className="focus:outline-none flex flex-col items-center">
+        <BsCalendar3 className="text-2xl" />
+        <span className="text-sm">Rdv</span>
+      </a>
+    </div>
+  );
+}
+
+export default MobileNavBar;
+
--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import MobileNavBar from './MobileNavBar';
+
 
 function NavBar() {
   const [isScrolled, setIsScrolled] = useState(false);
-  const [menuOpen, setMenuOpen] = useState(false);
+ 
 
   const handleScroll = () => {
     const currentScrollY = window.scrollY;
@@ -16,9 +18,7 @@ function NavBar() {
     };
   }, []);
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
+
 
   return (
     <nav className={`fixed w-full z-10 ${isScrolled ? 'bg-white' : 'bg-transparent'} `}>
@@ -50,57 +50,10 @@ function NavBar() {
                   </div>
             </a>
           </div>
-          {/* Mobile menu button */}
-          <button
-            className="md:hidden focus:outline-none"
-            onClick={toggleMenu}
-          >
-            {/* Menu icon */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
         </div>
       </div>
-      {/* Mobile menu */}
-      <div className={`md:hidden ${menuOpen ? 'block' : 'hidden'}`}>
-        <div className="bg-white">
-          <div className="container mx-auto px-4">
-            <div className="py-2 space-y-1">
-              <a href="#accueil" className="block px-3 py-2 text-sm font-medium">
-                ACCUEIL
-                <div className="relative">
-                    <div className="line absolute bottom-0 left-0 w-full h-0.5 bg-black transform scale-x-0 transition-transform duration-200"></div>
-                  </div>
-              </a>
-              <a href="#contact" className="block px-3 py-2 text-sm font-medium">
-                CONTACT
-                <div className="relative">
-                    <div className="line absolute bottom-0 left-0 w-full h-0.5 bg-black transform scale-x-0 transition-transform duration-200"></div>
-                  </div>
-              </a>
-              <a href="#rdv" className="block px-3 py-2 text-sm font-medium">
-                PRISE DE RDV
-                <div className="relative">
-                    <div className="line absolute bottom-0 left-0 w-full h-0.5 bg-black transform scale-x-0 transition-transform duration-200"></div>
-                  </div>
-              </a>
-            </div>
-          </div>
-        </div>
-            </div>
-          
+
+            <MobileNavBar />
     </nav>
   );
 }
