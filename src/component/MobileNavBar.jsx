575655c03c2a6e404e432a349e4dfd02c68f78fc
JOSEPH <lena.boya26@gmail.com>
2023-03-15 16:36:51 +0100

    anchor

--- src/component/MobileNavBar.jsx
+++ src/component/MobileNavBar.jsx
@@ -6,15 +6,15 @@ import { BsCalendar3 } from 'react-icons/bs';
 function MobileNavBar() {
   return (
     <div className="fixed bottom-0 left-0 w-full p-4 md:hidden bg-white rounded-t-lg shadow-lg flex justify-around items-center">
-      <a href="#" className="focus:outline-none flex flex-col items-center">
+      <a href="Accueil" className="focus:outline-none flex flex-col items-center">
         <AiOutlineHome className="text-2xl" />
         <span className="text-sm">Accueil</span>
       </a>
-      <a href="#" className="focus:outline-none flex flex-col items-center">
+      <a href="Contact" className="focus:outline-none flex flex-col items-center">
         <GrNotes className="text-2xl" />
         <span className="text-sm">Contact</span>
       </a>
-      <a href="#" className="focus:outline-none flex flex-col items-center">
+      <a href="Rdv" className="focus:outline-none flex flex-col items-center">
         <BsCalendar3 className="text-2xl" />
         <span className="text-sm">Rdv</span>
       </a>
